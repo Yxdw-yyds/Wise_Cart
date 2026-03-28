7c48e6944297076d9fac14b7d3e4faef6493c320
Lenovo <Lenovo@kesplus.com.cn>
2026-03-28 10:11:55 +0800

    feat: 添加 CCDRec 可视化页面

---
+++ src/views/model-expo/ccdrec-visualization/index.vue
@@ -0,0 +1,17 @@
+﻿<template>
+  <CcdrecVisualization />
+</template>
+
+<script setup>
+import { defineRouteMeta } from "@kesplus/kesplus";
+import CcdrecVisualization from "@/components/model-expo/ccdrec-visualization.vue";
+
+defineOptions({ name: "ModelExpoCcdrecVisualizationPage" });
+
+defineRouteMeta({
+  name: "modelExpoCcdrecVisualization",
+  title: "算法流程图",
+  icon: "DataAnalysis",
+  isKeepAlive: true,
+});
+</script>
